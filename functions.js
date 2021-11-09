47b3fb4354cf23721a540f788b1a8e7c05b7ac50
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-11-08 21:29:28 -0500

    fixing the buttons and alerts

--- functions.js
+++ functions.js
@@ -103,68 +103,75 @@ window.onload = function () {
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("Small Honeysuckle!"); 
-      })
+      });
 
       document.querySelector(".medium-scale-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("Medium Honeysuckle!");
-      })
+      });
 
       document.querySelector(".large-scale-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("Large Honeysuckle!");
-      })
+      });
 
       document.querySelector(".radial-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("You chose the Radial petal orientation"); 
-      })
+      });
 
       document.querySelector(".two21-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("You chose the 2+2+1 petal orientation"); 
-      })
+      });
       
       document.querySelector(".four1-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("You chose the 4+1 petal orientation"); 
-      })
+      });
 
       document.querySelector(".pendulus-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("You chose the Pendulus flower orientation"); 
-      })
+      });
 
       document.querySelector(".upright-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("You chose the Pendulus flower orientation"); 
-      })
+      });
 
       document.querySelector(".fused-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
         alert("Your honeysuckle will have Fused leaves"); 
-      })
+      });
 
-      document.querySelector(".unfused-button").addEventListener("click", function () {
+      document.querySelector(".red-button").addEventListener("click", function () {
         // here you can change also a-scene or a-entity properties, like
         // changing your 3D model source, size, position and so on
         // or you can just open links, trigger actions...
-        alert("Your honeysuckle will have Unfused leaves"); 
-      })
+        alert("Your honeysuckle is now red!"); 
+      });
+
+      document.querySelector(".blue-button").addEventListener("click", function () {
+        // here you can change also a-scene or a-entity properties, like
+        // changing your 3D model source, size, position and so on
+        // or you can just open links, trigger actions...
+        alert("Your honeysuckle is now blue!"); 
+      });
   }
