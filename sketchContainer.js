991000968e2e5410c7f2a3bb4409b36801a004f4
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-12-06 18:52:55 -0500

    going WAY BACK

--- sketchContainer.js
+++ sketchContainer.js
@@ -1,68 +1,28 @@
 // create a variable to hold our world object
 var world;
-
 // create a variable to hold our marker
-var marker1, marker2, marker3;
-
+var markerSize, markerPetal, markerFlower, markerLeaf, markerColor;
 // create variables to hold each plane
 var plane1, plane2, plane3, plane4, plane5;
-
 let p5graphics;
-
 let x1, x2, x3, x4, y1, y2, y3, y4, r, posNeg;
-
 let sprouts;
-
 let parts;
 
 let leaf, flower;
 
+let bounceAmount, bounceRate, previousRotation;
+
 var setLeafType;
 
 var setFlowerType;
-
 /*
-<img src="images/AR Media/Radial/radial_pendulus_large-01.svg" id="radial_pendulus_large-01">
-            <img src="images/AR Media/Radial/radial_pendulus_medium-01.svg" id="radial_pendulus_medium-01">
-            <img src="images/AR Media/Radial/radial_pendulus_small-01.svg" id="radial_pendulus_small-01">
-
-            <img src="images/AR Media/Radial/radial_upright_large-01.svg" id="radial_upright_large-01">
-            <img src="images/AR Media/Radial/radial_upright_medium-01.svg" id="radial_upright_medium-01">
-            <img src="images/AR Media/Radial/radial_upright_small-01.svg" id="radial_upright_small-01">
-            
-            <!-- 2+2+1 Petal Orientation -->
-            <img src="images/AR Media/221/221_pendulus_large-01.svg" id="221_pendulus_large-01">
-            <img src="images/AR Media/221/221_pendulus_medium-01.svg" id="221_pendulus_medium-01">
-            <img src="images/AR Media/221/221_pendulus_small-01.svg" id="221_pendulus_small-01">
-
-            <img src="images/AR Media/221/221_upright_large-01.svg" id="221_upright_large-01">
-            <img src="images/AR Media/221/221_upright_medium-01.svg" id="221_upright_medium-01">
-            <img src="images/AR Media/221/221_upright_small-01.svg" id="221_upright_small-01">
-
-            <!-- 4+1 Petal Orientation -->
-            <img src="images/AR Media/41/41_pendulus_large-01.svg" id="41_pendulus_large-01">
-            <img src="images/AR Media/41/41_pendulus_medium-01.svg" id="41_pendulus_medium-01">
-            <img src="images/AR Media/41/41_pendulus_small-01.svg" id="41_pendulus_small-01">
-
-            <img src="images/AR Media/41/41_upright_large-01.svg" id="41_upright_large-01">
-            <img src="images/AR Media/41/41_upright_medium-01.svg" id="41_upright_medium-01">
-            <img src="images/AR Media/41/41_upright_small-01.svg" id="41_upright_small-01">
-
-            <!-- Unfused Leaf Fusion -->
-            <img src="images/AR Media/unfused_leaves-01.svg" id="unfused_leaves-01">
-
-            <!-- Fused Leaf Fusion -->
-            <img src="images/AR Media/fused_leaves-01.svg" id="fused_leaves-01">
-              
-            </img>
-
 // arrays for buttons/UI
 var leafShape = [leaf];
 var leafSize = [60, 40, 80];
 var flowerShape = [flower];
 var flowerSize = [60, 40, 80];
 var flowerUp = [true, false];
-
 // variables for contructors
 var setLeafShape;
 var setLeafSize;
@@ -70,27 +30,29 @@ var setFlowerShape;
 var setFlowerSize;
 var setFlowerDown;
 */
-
 function preload(){
   //leaf = setLeafType;
   //flower = setFlowerType;
-
   leaf = loadImage("images/leaf.svg");
   flower = loadImage("images/flower.svg");
 }
-
 function setup() {
-
+    // create our world (this also creates a p5 canvas for us)
+    world = new World('ARScene');
+    // grab a reference to the marker that we set up on the HTML side (connect to it using its 'id')
+    markerSize = world.getMarker('patt1');
+    markerPetal = world.getMarker('patt2');
+    markerFlower = world.getMarker('patt3');
+    markerLeaf = world.getMarker('patt4');
+    markerColor = world.getMarker('patt5');
     p5graphics = createGraphics(600, 600).id('p5graphics');
     angleMode(degrees);
     createCanvas(600, 600);
     sprouts = [];
     parts = [];
     createVine();
-
     let rotationRange = 10;
     let p;
-
     for (let s = 0; s < sprouts.length; s++) {
       p = new Part(
         "leaf",
@@ -142,27 +104,12 @@ function setup() {
       );
       parts.push(p);
     }
-
-  // create our world (this also creates a p5 canvas for us)
-  world = new World('ARScene');
-
-  // grab a reference to the marker that we set up on the HTML side (connect to it using its 'id')
-  marker1 = world.getMarker('patt1');
-  marker2 = world.getMarker('patt2');
-  marker3 = world.getMarker('patt3');
-  //marker4 = world.getMarker('patt4');
-  //marker5 = world.getMarker('patt5');
-  //marker6 = world.getMarker('patt6');
-  //marker3 = world.getMarker('hiro');
-
-
   // create some geometry to add to our marker
   // the marker is 1 meter x 1 meter, with the origin at the center
   // the x-axis runs left and right
   // -0.5, 0, -0.5 is the top left corner
-
-container = new Box({
-    x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
+container1 = new Box({
+  x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
 });
 
 container2 = new Box({
@@ -173,14 +120,19 @@ container3 = new Box({
   x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
 });
 
-marker1.addChild( container );
-marker2.addChild( container2 );
-marker3.addChild( container3 );
+container4 = new Box({
+  x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
+});
 
-//marker3.addChild( container );
-//marker4.addChild( container );
-//marker5.addChild( container );
-//marker6.addChild( container );
+container5 = new Box({
+  x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
+});
+
+markerSize.addChild( container1 );
+markerPetal.addChild( container2 );
+markerFlower.addChild( container3 );
+markerLeaf.addChild( container4 );
+markerColor.addChild( container5 );
 
 // intent behing this 'container" box was that *it* would rotate to face the camera using a single line of code,
 // rather than rotating every plane contained in the box individually.
@@ -196,7 +148,7 @@ marker3.addChild( container3 );
     // https://p5js.org/reference/#/p5.Graphics
     // find the simple code drawing random ellipsed in the draw loop below
   });
-  container.addChild( plane1 );
+  container1.addChild( plane1 );
 
   plane2 = new Plane({
     x:0, y:0, z:1.0,
@@ -219,35 +171,49 @@ marker3.addChild( container3 );
     // find the simple code drawing random ellipsed in the draw loop below
   });
   container3.addChild( plane3 );
+
+  plane4 = new Plane({
+    x:0, y:0, z:1.0,
+    //red:0, green:0, blue:255,
+    width:1, height:1,
+    asset:'p5graphics' 
+    // this plane is textured by a p5.graphics object
+    // https://p5js.org/reference/#/p5.Graphics
+    // find the simple code drawing random ellipsed in the draw loop below
+  });
+  container4.addChild( plane4 );
+
+  plane5 = new Plane({
+    x:0, y:0, z:1.0,
+    //red:0, green:0, blue:255,
+    width:1, height:1,
+    asset:'p5graphics' 
+    // this plane is textured by a p5.graphics object
+    // https://p5js.org/reference/#/p5.Graphics
+    // find the simple code drawing random ellipsed in the draw loop below
+  });
+  container5.addChild( plane5 );
+
 }
-
-
 function draw() {
-
     world.clearDrawingCanvas();
-
     // get the rotation of the AR marker relative to the camera,
     // which allows us to rotate the objects attached to that marker toward the camera
-    let rot = marker1.tag.object3D.rotation;
-    let rot2 = marker2.tag.object3D.rotation;
-    let rot3 = marker3.tag.object3D.rotation;
+    let rot1 = markerSize.tag.object3D.rotation;
+    let rot2 = markerPetal.tag.object3D.rotation;
+    let rot3 = markerFlower.tag.object3D.rotation;
+    let rot4 = markerLeaf.tag.object3D.rotation;
+    let rot5 = markerColor.tag.object3D.rotation;
 
-   // let rot4 = marker4.tag.object3D.rotation;
-   // let rot5 = marker5.tag.object3D.rotation;
-   // let rot6 = marker6.tag.object3D.rotation;
-
-    let normalRot = degrees(rot.z);
+    let normalRot1 = degrees(rot1.z);
     let normalRot2 = degrees(rot2.z);
     let normalRot3 = degrees(rot3.z);
-    //let normalRot2 = degrees(rot2.z);// + Math.ceil(-1 * degrees(rot.y) / 360) * 360;
-    //let normalRot3 = degrees(rot3.z);
-    //let normalRot4 = degrees(rot4.z);// + Math.ceil(-1 * degrees(rot.y) / 360) * 360;
-   // let normalRot5 = degrees(rot5.z);
+    let normalRot4 = degrees(rot4.z);
+    let normalRot5 = degrees(rot5.z);
 
     p5graphics.noFill();
     p5graphics.strokeWeight(4);
     p5graphics.stroke(0, 200, 0);
-
     p5graphics.bezier(x1, y1, x2, y2, x3, y3, x4, y4);
     p5graphics.fill(255);
 
@@ -257,38 +223,24 @@ function draw() {
       // p5graphics.image(leaf, sprouts[s][0], sprouts[s][1], 60, 60);
     }
 
-
     // p5graphics.image(flower, sprouts[0][0], sprouts[0][1], 60, 60);
-
     for (let p = 0; p < parts.length; p++) {
       parts[p].show();
     }
 
     p5graphics.background(0,0,0,0);
-
     //plane4.rotateY(plane4.getRotationY()+2);
-
    //var markerPosition = marker.getScreenPosition();
-
     //var markerRotation = marker.getRotationY();
-
-    
-
+  
     // container.rotateY(normalRot);
     // should be able to just rotate the containing box, but it ain't working
     // so have to rotate the individual objects (planes) instead
-    
 
-    //plane1.rotateY(normalRot);
-    //plane2.rotateY(normalRot2);
-    //plane3.rotateY(normalRot3);
-
-  if (marker1.isVisible() == true) 
+  if (markerSize.isVisible() == true) // SIZE TAB
   {
         //document.querySelector(".size-button").style.visibility = "visible";
         //document.querySelector(".size-button").style.display = "flex";
-        document.querySelector(".navButtons").style.visibility = "visible";
-        document.querySelector(".navButtons").style.display = "flex";
         document.querySelector(".sizeButtons").style.visibility = "visible";
         document.querySelector(".sizeButtons").style.display = "flex";
         document.querySelector(".petalOrientButtons").style.visibility = "hidden";
@@ -299,14 +251,13 @@ function draw() {
         document.querySelector(".leafFusionButtons").style.display = "none";
         document.querySelector(".colorButtons").style.visibility = "hidden";
         document.querySelector(".colorButtons").style.display = "none";
-        plane1.rotateY(normalRot);
-  }
-  if (marker2.isVisible() == true) 
+        plane1.rotateY(normalRot1);
+  };
+
+  if (markerPetal.isVisible() == true) // PETAL ORIENTATION TAB
   {
         //document.querySelector(".petal-button").style.visibility = "visible";
         //document.querySelector(".petal-button").style.display = "flex";
-        document.querySelector(".navButtons").style.visibility = "visible";
-        document.querySelector(".navButtons").style.display = "flex";
         document.querySelector(".sizeButtons").style.visibility = "hidden";
         document.querySelector(".sizeButtons").style.display = "none";
         document.querySelector(".petalOrientButtons").style.visibility = "visible";
@@ -318,13 +269,12 @@ function draw() {
         document.querySelector(".colorButtons").style.visibility = "hidden";
         document.querySelector(".colorButtons").style.display = "none";
         plane2.rotateY(normalRot2);
-  }
-  if (marker3.isVisible() == true)  // FLOWER TAB
+  };
+
+  if (markerFlower.isVisible() == true)  // FLOWER ORIENTATION TAB
   {
       //document.querySelector(".flower-button").style.visibility = "visible";
       //document.querySelector(".flower-button").style.display = "flex";
-      document.querySelector(".navButtons").style.visibility = "visible";
-      document.querySelector(".navButtons").style.display = "flex";
       document.querySelector(".sizeButtons").style.visibility = "hidden";
       document.querySelector(".sizeButtons").style.display = "none";
       document.querySelector(".petalOrientButtons").style.visibility = "hidden";
@@ -336,12 +286,14 @@ function draw() {
       document.querySelector(".colorButtons").style.visibility = "hidden";
       document.querySelector(".colorButtons").style.display = "none";
       plane3.rotateY(normalRot3);
-  }
-  /*
-  if (marker4.isVisible() == true)  // LEAF FUSION TAB
+  };
+  
+  if (markerLeaf.isVisible() == true)  // LEAF FUSION TAB
   {
+    /*
       document.querySelector(".leaf-button").style.visibility = "hidden";
       document.querySelector(".leaf-button").style.display = "none";
+      */
       document.querySelector(".sizeButtons").style.visibility = "hidden";
       document.querySelector(".sizeButtons").style.display = "none";
       document.querySelector(".petalOrientButtons").style.visibility = "hidden";
@@ -352,11 +304,15 @@ function draw() {
       document.querySelector(".leafFusionButtons").style.display = "flex";
       document.querySelector(".colorButtons").style.visibility = "hidden";
       document.querySelector(".colorButtons").style.display = "none";
-  }
-  if (marker5.isVisible() == true)  // COLOR TAB
+      plane4.rotateY(normalRot4);
+  };
+
+  if (markerColor.isVisible() == true)  // COLOR TAB
   { 
+    /*
       document.querySelector(".color-button").style.visibility = "hidden";
       document.querySelector(".color-button").style.display = "none";
+      */
       document.querySelector(".sizeButtons").style.visibility = "hidden";
       document.querySelector(".sizeButtons").style.display = "none";
       document.querySelector(".petalOrientButtons").style.visibility = "hidden";
@@ -366,38 +322,25 @@ function draw() {
       document.querySelector(".leafFusionButtons").style.visibility = "hidden";
       document.querySelector(".leafFusionButtons").style.display = "none";
       document.querySelector(".colorButtons").style.visibility = "visible";
-      document.querySelector(".colorButtons").style.display = "flex";
-  )
-  if (marker6.isVisible() == true) // GROWTH TAB
-  { 
-      document.querySelector(".growth-button").style.visibility = "hidden";
-      document.querySelector(".growth-button").style.display = "none";
-      document.querySelector(".sizeButtons").style.visibility = "hidden";
-      document.querySelector(".sizeButtons").style.display = "none";
-      document.querySelector(".petalOrientButtons").style.visibility = "hidden";
-      document.querySelector(".petalOrientButtons").style.display = "none";
-      document.querySelector(".flowerOrientButtons").style.visibility = "hidden";
-      document.querySelector(".flowerOrientButtons").style.display = "none";
-      document.querySelector(".leafFusionButtons").style.visibility = "hidden";
-      document.querySelector(".leafFusionButtons").style.display = "none";
-      document.querySelector(".colorButtons").style.visibility = "visible";
-      document.querySelector(".colorButtons").style.display = "flex";
-  }*/
-  else
+      document.querySelector(".colorButtons").style.display = "grid";
+      plane5.rotateY(normalRot5);
+  };
+  
+  if(markerSize.isVisible() == false && markerPetal.isVisible() == false && markerFlower.isVisible() == false && markerLeaf.isVisible() == false && markerColor.isVisible() == false)
   {
-        document.querySelector(".navButtons").style.visibility = "hidden";
-        document.querySelector(".navButtons").style.display = "none";
-        document.querySelector(".sizeButtons").style.visibility = "hidden";
-        document.querySelector(".sizeButtons").style.display = "none";
-        document.querySelector(".petalOrientButtons").style.visibility = "hidden";
-        document.querySelector(".petalOrientButtons").style.display = "none";
-        document.querySelector(".flowerOrientButtons").style.visibility = "hidden";
-        document.querySelector(".flowerOrientButtons").style.display = "none";
-        document.querySelector(".leafFusionButtons").style.visibility = "hidden";
-        document.querySelector(".leafFusionButtons").style.display = "none";
-        document.querySelector(".colorButtons").style.visibility = "hidden";
-        document.querySelector(".colorButtons").style.display = "none";
-  }
+      document.querySelector(".navButtons").style.visibility = "hidden";
+      document.querySelector(".navButtons").style.display = "none";
+      document.querySelector(".sizeButtons").style.visibility = "hidden";
+      document.querySelector(".sizeButtons").style.display = "none";
+      document.querySelector(".petalOrientButtons").style.visibility = "hidden";
+      document.querySelector(".petalOrientButtons").style.display = "none";
+      document.querySelector(".flowerOrientButtons").style.visibility = "hidden";
+      document.querySelector(".flowerOrientButtons").style.display = "none";
+      document.querySelector(".leafFusionButtons").style.visibility = "hidden";
+      document.querySelector(".leafFusionButtons").style.display = "none";
+      document.querySelector(".colorButtons").style.visibility = "hidden";
+      document.querySelector(".colorButtons").style.display = "none";
+  };
 }
 
 function createVine() {
@@ -411,7 +354,6 @@ function createVine() {
   y4 = height - 2 * (height / 3);
   y2 = (y4 - y1) / 3 + y1;
   y3 = y4 - (y4 - y1) / 3;
-
   let steps = 4; // number of sprout points between bottom-most and top-most point
   for (let i = 0; i <= steps; i++) {
     let t = i / steps;
@@ -420,8 +362,6 @@ function createVine() {
     sprouts.push([x, y]);
   }
 }
-
-let bounceAmount, bounceRate, previousRotation;
 
 class Part {
   constructor(
@@ -446,46 +386,35 @@ class Part {
     this.isRight = isRight;
     this.isDown = isDown;
     this.color = color;
-
     bounceAmount = 1 + random(2, 4);
     print("bounceAmount: " + bounceAmount);
     bounceRate = random(-0.025, 0.025);
     print("bounceRate: " + bounceRate);
-
     if (isRight == false) {
       bounceRate = -bounceRate;
     }
-
     // if (isRight == true) {
     //   bounceRate = .02;
     // } else {
     //   bounceRate = -.02;
     // }
-
     previousRotation = this.rotation;
   }
-
   show() {
     p5graphics.push();
-
     p5graphics.translate(this.posX, this.posY);
-
     if (this.isRight == false) {
       p5graphics.scale(-1, 1);
     }
     if (this.isDown == false) {
       p5graphics.scale(1, -1);
     }
-
     // if (this.name == "flower") {
     //   this.bounce();
     // }
-
     p5graphics.image(this.img, 0, 0, this.width, this.height);
-
     p5graphics.pop();
   }
-
   bounce() {
     let b = previousRotation + bounceRate;
     rotate(b);
