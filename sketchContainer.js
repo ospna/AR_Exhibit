a8a0bbd3ee74999f9107f5e9d433314cb65dd1b3
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2021-12-12 16:50:05 -0500

    lets test out these 3d models

--- sketchContainer.js
+++ sketchContainer.js
@@ -2,7 +2,7 @@
 var world;
 
 // create a variable to hold our marker
-var markerSize, markerPetal, markerFlower, markerLeaf, markerColor;
+var markerSize, markerPetal, markerFlower, markerLeaf, markerColor, markerPaper;
 
 // create variables to hold each plane
 var plane1, plane2, plane3, plane4, plane5;
@@ -57,6 +57,7 @@ function setup() {
     markerFlower = world.getMarker('patt3');
     markerLeaf = world.getMarker('patt4');
     markerColor = world.getMarker('patt5');
+    markerPaper = world.getMarker('patt6');
 
     p5graphics = createGraphics(600, 600).id('p5graphics');
     angleMode(degrees);
@@ -90,11 +91,16 @@ container5 = new Box({
   x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
 });
 
+container6 = new Box({
+  x:0, y:0, z:0, width: 1, depth: 1, height: 1,rotationX:270.0,opacity:0
+});
+
 markerSize.addChild( container1 );
 markerPetal.addChild( container2 );
 markerFlower.addChild( container3 );
 markerLeaf.addChild( container4 );
 markerColor.addChild( container5 );
+markerPaper.addChild( container6 );
 
 // intent behing this 'container" box was that *it* would rotate to face the camera using a single line of code,
 // rather than rotating every plane contained in the box individually.
@@ -156,6 +162,17 @@ markerColor.addChild( container5 );
   });
   container5.addChild( plane5 );
 
+  plane6 = new Plane({
+    x:0, y:0, z:1.0,
+    //red:0, green:0, blue:255,
+    width:1, height:1,
+    asset:'p5graphics' 
+    // this plane is textured by a p5.graphics object
+    // https://p5js.org/reference/#/p5.Graphics
+    // find the simple code drawing random ellipsed in the draw loop below
+  });
+  container6.addChild( plane6 );
+
 }
 
 function draw() {
@@ -169,12 +186,14 @@ function draw() {
     let rot3 = markerFlower.tag.object3D.rotation;
     let rot4 = markerLeaf.tag.object3D.rotation;
     let rot5 = markerColor.tag.object3D.rotation;
+    let rot6 = markerPaper.tag.object3D.rotation;
 
     let normalRot1 = degrees(rot1.z);
     let normalRot2 = degrees(rot2.z);
     let normalRot3 = degrees(rot3.z);
     let normalRot4 = degrees(rot4.z);
     let normalRot5 = degrees(rot5.z);
+    let normalRot6 = degrees(rot6.z);
 
     p5graphics.noFill();
     p5graphics.strokeWeight(4);
@@ -290,6 +309,30 @@ function draw() {
       document.querySelector(".colorButtons").style.display = "grid";
       plane5.rotateY(normalRot5);
   };
+
+  if (markerPaper.isVisible() == true)  // 3D Model Tester
+  { 
+    /*
+      document.querySelector(".color-button").style.visibility = "hidden";
+      document.querySelector(".color-button").style.display = "none";
+      */
+      document.querySelector(".navButtons").style.visibility = "hidden";
+      document.querySelector(".navButtons").style.display = "none";
+      document.querySelector(".sizeButtons").style.visibility = "hidden";
+      document.querySelector(".sizeButtons").style.display = "none";
+      document.querySelector(".petalOrientButtons").style.visibility = "hidden";
+      document.querySelector(".petalOrientButtons").style.display = "none";
+      document.querySelector(".flowerOrientButtons").style.visibility = "hidden";
+      document.querySelector(".flowerOrientButtons").style.display = "none";
+      document.querySelector(".leafFusionButtons").style.visibility = "hidden";
+      document.querySelector(".leafFusionButtons").style.display = "none";
+      document.querySelector(".colorButtons").style.visibility = "hidden";
+      document.querySelector(".colorButtons").style.display = "none";
+      document.querySelector(".3d_models").style.visibility = "visible";
+      //document.querySelector(".3d_models").style.display = "none";
+      plane6.rotateY(normalRot6);
+  };
+  
   
   if(markerSize.isVisible() == false && markerPetal.isVisible() == false && markerFlower.isVisible() == false && markerLeaf.isVisible() == false && markerColor.isVisible() == false)
   {
@@ -305,6 +348,7 @@ function draw() {
       document.querySelector(".leafFusionButtons").style.display = "none";
       document.querySelector(".colorButtons").style.visibility = "hidden";
       document.querySelector(".colorButtons").style.display = "none";
+
   };
 }
 
